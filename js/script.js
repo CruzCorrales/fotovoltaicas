77fbee23cc7616980b78b46a31a2ff7f9c13f2a5
CruzCorrales <cruzcorralesramos@gmail.com>
2022-09-29 17:47:02 +0200

    aportando javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const ocultarPaneles = document.querySelector(".main__paneles");
+const ocultarBaterias = document.querySelector(".main__baterias");
+const ocultarTransformadores = document.querySelector(".main__transformadores");
+const divParteArray = document.querySelectorAll(".main__article");
+
+ocultarPaneles.addEventListener("click", ocultar2y3)
+function ocultar2y3 (){
+    divParteArray[1].style.display = "none";
+    divParteArray[2].style.display = "none";
+}
+ocultarBaterias.addEventListener("click", ocultar1y3)
+function ocultar1y3 (){
+    divParteArray[0].style.display = "none";
+    divParteArray[2].style.display = "none";
+}
+ocultarTransformadores.addEventListener("click", ocultar1y2)
+function ocultar1y2 (){
+    divParteArray[0].style.display = "none";
+    divParteArray[1].style.display = "none";
+}
